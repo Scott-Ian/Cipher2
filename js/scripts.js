33dc5f1d0c0ca93d6430ac13bdcab4557880b2a4
Ian Scott <chesnekov@gmail.com>
2020-06-01 12:02:22 -0700

    basic JS functionality
    Co-authored-by: Payne Fulcher <pfulcher26@gmail.com>
    Co-authored-by: Peter Grimm <pagrimm@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,25 @@
 $(document).ready(function(){
 
+  //Prompts user for a centence, then logs to console
+  const userSentence = prompt("Please enter a sentence to be ciphered:");
+  console.log(userSentence);
 
+  //Acquires user input sentence length
+  const sentenceLength=userSentence.length;
+  console.log(sentenceLength);
+  
+  const firstChar = 0;
+  const lastChar = sentenceLength -1;
+
+  //Capitalizes the first and last letter in the sentence and returns
+  const firstLastCapital = function(userSentence){
+    const capitalFirst = toUpper(userSentence.charAt(firstChar));
+    const capitalLast = toUpper(userSentence.charAt(lastChar));
+
+    console.log(capitalFirst + capitalLast);
+    return capitalFirst + capitalLast;
+  };
 
 });
+
+// .charAt(number);
